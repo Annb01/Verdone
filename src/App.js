825e423123d24a75fcff63ed56aae42b04c85e96
Annb01 <bubula.ann1@gmail.com>
2025-05-11 14:47:08 +0200

    Create app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "../src/App.css";
+function App() {
+ 
+
+  return (
+    <>
+    <div className="todo-front-page">
+        <nav className="navbar-1">
+            <button className="back-button" >◀</button>
+            <div className="truth-or-dare">
+                <h3>Prawda Czy Wyzwanie</h3>
+            </div>
+        </nav>
+
+
+        <main className="main">
+            
+        </main>
+    </div>
+
+    
+  </>
+  );
+}
+
+export default App;
